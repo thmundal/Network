c4ead4c67647461d6b50ca5ba4574c8227dad0c3
Thomas Mundal <thmundal@gmail.com>
2017-10-30 16:05:38 +0100

    Basic network communication test success

--- sfml_test/main.cpp
+++ sfml_test/main.cpp
@@ -1,11 +1,23 @@
+#pragma comment(lib, "sfml-network.lib");
 #include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Network/IpAddress.hpp>
+
 #include <iostream>
 
 int main()
 {
+
+	sf::TcpSocket socket;
+	socket.connect("192.168.1.43", 23);
+
+
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	char buffer[256];
+	std::size_t received = 0;
 
 	while (window.isOpen())
 	{
@@ -15,6 +27,9 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		socket.receive(buffer, sizeof(buffer), received);
+		std::cout << "server said " << buffer << std::endl;
 
 		window.clear();
 		window.draw(shape);
