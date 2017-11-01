151bb4b430a5f09daeb9ed8ae4b43dec4917b650
Thomas Mundal <thmundal@gmail.com>
2017-11-01 09:40:29 +0100

    Added Arduino project

---
+++ ArduinoServer/Debug/ArduinoServer.cpp.d
@@ -0,0 +1,39 @@
+C:\Users\Thomas\AppData\Local\Temp\VMBuilds\ArduinoServer\mega_atmega2560\Debug\ArduinoServer.cpp.o: \
+ C:\Users\Thomas\AppData\Local\Temp\VMBuilds\ArduinoServer\mega_atmega2560\Debug\ArduinoServer.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/UIPEthernet.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/ethernet_comp.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/Dhcp.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/UIPUdp.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Udp.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/mempool.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/mempool_conf.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/uipethernet-conf.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/uipopt.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/uip-conf.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/enc28j60.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/uip.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/Enc28J60Network.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/mempool.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/UIPClient.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Client.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/IPAddress.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/UIPServer.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Server.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/uip_timer.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/uip_clock.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\UIPEthernet/utility/clock-arch.h
